530bd4dca594d9c14b0d6ee6f53a56e92b26fc0e
YuanX1a0 <YuanX1a0@users.noreply.github.com>
2026-08-17 17:33:46 +0800

    Bump version to 5.1.0

--- Assistant/Controllers/AppController.cs
+++ Assistant/Controllers/AppController.cs
@@ -9,7 +9,7 @@ namespace Assistant.Controllers
 {
     public static class AppController
     {
-        public const string AssemblyVersion = "5.0.1";
+        public const string AssemblyVersion = "5.1.0";
         public static readonly string Version = "v" + AssemblyVersion;
         public const bool IsBetaVersion = false;
         public static bool CanFollowSystemColor = false;
